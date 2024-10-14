f4419e16424eb0f3fae2f088e96783dc16b24abf
omrawat23 <omrawat23@gmail.com>
2024-10-14 06:52:28 +0530

    button updated

--- src/components/Widget.jsx
+++ src/components/Widget.jsx
@@ -1,6 +1,6 @@
 
 import { useState } from "react"
-import { Button } from "../components/ui/button"
+import { Button } from "./ui/Button"
 import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
